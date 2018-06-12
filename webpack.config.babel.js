ebbd9c8cf1b5b0a068dd569a59ed6eda3b64c404
田中将平 <syouhei@jobby-MBP.local>
2018-06-12 23:42:40 +0900

    create project

---
+++ webpack.config.babel.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'public');
+
+export default {
+  mode: 'development',
+  entry: src + '/index.js',
+
+  output: {
+    path: dist,
+    filename: `bundle.js`,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
